b8dec0ad98fe4fec4891e6b8ba481a374846069a
성인 김 <99_insung@naver.com>
2021-09-18 18:15:17 +0900

    Commit

---
+++ CarniCoin/CarniCoin.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Coin_n[2] = { 1, 2 };
+//동전은 하나 또는 두개를 뒤집을 수 있음.
+int Carnicoin(int n) {
+	int student, customer;
+
+	scanf("firstable customer %d", &customer);
+
+	if (customer > 2 && customer < 1) {
+		fprintf(stderr, -1, "input err");
+	}
+
+	else if (customer == 1) {
+
+	}
+
+	else //custoemr == 2 
+
+	
+
+}
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	Carnicoin(num);
+}
